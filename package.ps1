9e689597aec46229d152725868d0e5cc7115c83c
SakiikaVR <267207995+SakiikaVR@users.noreply.github.com>
2026-09-25 08:44:20 +0900

    Prepare v1.4 release

--- package.ps1
+++ package.ps1
@@ -3,7 +3,7 @@ Add-Type -AssemblyName System.IO.Compression
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
 $root = $PSScriptRoot
-$version = '1.3'
+$version = '1.4'
 $releaseDir = Join-Path $root 'release'
 New-Item -ItemType Directory -Force -Path $releaseDir | Out-Null
 $zipPath = Join-Path $releaseDir "JIZURA-AviUtl2-v$version.zip"
